ef51e4540c3defe199b3923240a1edb5cb03c717
Christophe Bédanne <christophe.bedanne@sfpd.fgov.be>
2020-12-02 14:43:33 +0100

    BabyStep First Commit

--- CaesarCipher/src/main/java/CaesarCipherEncoder.java
+++ CaesarCipher/src/main/java/CaesarCipherEncoder.java
@@ -1,8 +1,19 @@
 public class CaesarCipherEncoder {
     public String encode(String input, int shift) {
-        if(shift==0){
-            return input;
-        }
-        return "Tjnqmf";
+//        char[] value  = input.toCharArray();
+//        for (int i =0 ; i<value.length; i++){
+//           char letter =value[i];
+////           letter = (char)letter+shift;
+//
+//        }
+
+
+//        if(shift==0){
+//            return input;
+//        }
+//        if(input.equals("A")){
+//            return input.c
+//        }
+        return "A";
     }
 }
--- CaesarCipher/src/test/java/CaesarCipherTest.java
+++ CaesarCipher/src/test/java/CaesarCipherTest.java
@@ -5,32 +5,71 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class CaesarCipherTest {
 
     /*
-    Shift is 0 => "Simple" ->  "Simple"
-    Shift is 1 => "Simple" -> "Tjnqmf" - Done
-    Shift is -1 => "A" ->"Z"
+    Shift is 1 => "Simple" -> "Tjnqmf" - On hold
+    Shift is 0 => "Simple" ->  "Simple" - On hold
+    Sift is -1 => "A" -> "Z" - On hold
+    Shift is 0 => "A" -> "A"
+    Shift is 0 => "B" -> "B"
+    Shift is 1 => "A" -> "B"
+    Shift is 1 => "B" -> "C"
+    Shift is 0 => "AA" -> "AA"
+    Shift is 1 => "AA" -> "BB"
+    Shift is 2 => "AAZ" -> "CCB"
      */
 
     @Test
-    public void when_SimpleAndShift1_Then_Tjnqmf(){
+    public void when_A_Offset0_A(){
+
         //Given
         CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder();
-        int shift = 1;
+        int shift = 0;
         //When
-        String result=caesarCipherEncoder.encode("Simple",shift);
-        //Then
-        assertThat(new String("Tjnqmf")).isEqualTo( result);
-    }
-
-    @Test
-    public void when_SimpleAndShift0_Then_Simple(){
-        //Given
-        CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder();
-        int shift=0;
-        //When
-        String result=caesarCipherEncoder.encode("Simple",shift);
+        String result = caesarCipherEncoder.encode("A", shift);
 
         //Then
-        assertThat("Simple").isEqualTo(result);
+        assertThat(result).
 
+        isEqualTo("A");
     }
+
+
+
+//    @Test
+//    public void when_SimpleAndShift1_Then_Tjnqmf(){
+//        //Given
+//        CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder();
+//        int shift = 1;
+//        //When
+//        String result=caesarCipherEncoder.encode("Simple",shift);
+//        //Then
+//        assertThat(result).isEqualTo("Tjnqmf");
+//    }
+//
+//    @Test
+//    public void when_SimpleAndShift0_Then_Simple(){
+//        //Given
+//        CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder();
+//        int shift=0;
+//        //When
+//        String result=caesarCipherEncoder.encode("Simple",shift);
+//
+//        //Then
+//        assertThat(result).isEqualTo("Simple");
+//    }
+
+//    @Test
+//    public void when_A_AndShift_minus1_Then_Z(){
+//        //Given
+//        CaesarCipherEncoder caesarCipherEncoder = new CaesarCipherEncoder();
+//        int shift=-1;
+//        //When
+//        String result=caesarCipherEncoder.encode("Simple",shift);
+//
+//        //Then
+//        assertThat(result).isEqualTo("Z");
+//    }
+
+
+
+
 }
