84851d2fe67d19893052551d3fd0e6e7b2d9b0cf
Christophe Bédanne <christophe.bedanne@sfpd.fgov.be>
2020-12-02 11:52:23 +0100

    Initial commit

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstTest {
+
+    @Test
+    public void testTrue(){
+        assertThat(Boolean.TRUE);
+    }
+}
