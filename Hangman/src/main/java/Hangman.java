bae51c40334195ddb0afe347d3ba0a199268d75e
Christophe Bédanne <christophe.bedanne@sfpd.fgov.be>
2020-12-03 12:01:28 +0100

    Hangman Test2

--- Hangman/src/main/java/Hangman.java
+++ Hangman/src/main/java/Hangman.java
@@ -1,9 +1,20 @@
 public class Hangman {
 
+    private String setSecretWord;
+
     public Hangman() {
     }
 
     public String checkLetter(String letter) {
+
+        if(setSecretWord.length()==2){
+            return "--";
+        }
         return "-";
+    }
+
+
+    public void setSecretWord(String secretWord){
+        this.setSecretWord=secretWord;
     }
 }
--- Hangman/src/test/java/HangManTest.java
+++ Hangman/src/test/java/HangManTest.java
@@ -18,13 +18,25 @@ public class HangManTest {
     public void when_A_Proposed_Nothing_Then_(){
         //Given
         String letter="";
+        String secretWord="A";
         //When
         Hangman hangman=new Hangman();
+        hangman.setSecretWord(secretWord);
         //Then
         assertThat(hangman.checkLetter(letter)).isEqualTo("-");
     }
 
-
+    @Test
+    public void when_AB_Proposed_Nothing_Then__(){
+        //Given
+        String letter="";
+        String secretWord="AB";
+        //When
+        Hangman hangman=new Hangman();
+        hangman.setSecretWord(secretWord);
+        //Then
+        assertThat(hangman.checkLetter(letter)).isEqualTo("--");
+    }
 
 
 
