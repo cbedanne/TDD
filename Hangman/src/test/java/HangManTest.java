97f33b7cf05ee5633435285a9d43a585af95e6e4
Christophe Bédanne <christophe.bedanne@sfpd.fgov.be>
2020-12-03 13:25:06 +0100

    Hangman Test4

--- Hangman/src/test/java/HangManTest.java
+++ Hangman/src/test/java/HangManTest.java
@@ -7,7 +7,7 @@ public class HangManTest {
     When SecretWord A proposed nothing _
     When SecretWord AB proposed nothing __
     When SecretWord A proposed A return A
-    When SecretWord A proposed B return _
+    When SecretWord A proposed B return -
     When SecretWord secret proposed e return -e--e-
     When SecretWord secret proposed g return ------
     When SecretWord secret completed word -e--e- proposed z return -e--e-
@@ -48,6 +48,18 @@ public class HangManTest {
         hangman.setSecretWord(secretWord);
         //Then
         assertThat(hangman.checkLetter(letter)).isEqualTo("A");
+    }
+
+    @Test
+    public void when_A_Proposed_B_Then__(){
+        //Given
+        String letter="B";
+        String secretWord="A";
+        //When
+        Hangman hangman=new Hangman();
+        hangman.setSecretWord(secretWord);
+        //Then
+        assertThat(hangman.checkLetter(letter)).isEqualTo("-");
     }
 
 
